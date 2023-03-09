9fd796c94043d33c4df10a0bbd4fb2a5a53e8df3
Rajesh Kumar Yadav <81347109+rajesh4289@users.noreply.github.com>
2023-03-09 11:21:11 +0530

    Formating and js

---
+++ JavaScript/password_checker.js
@@ -0,0 +1,11 @@
+const passwordInput = document.getElementById("password");
+const confirmPasswordInput = document.getElementById("confirm-password");
+const passwordError = document.getElementById("password-error");
+
+confirmPasswordInput.addEventListener("input", function() {
+  if (passwordInput.value !== confirmPasswordInput.value) {
+    passwordError.textContent = "Passwords do not match.";
+  } else {
+    passwordError.textContent = "";
+  }
+});
